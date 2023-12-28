8902c3a953ccba8a4ff194780f6259b0beda6c90
Justin Guese <guese.justin@gmail.com>
2023-12-28 14:49:17 +0100

    delete old files fnctionality

--- flows/databaseBackup.py
+++ flows/databaseBackup.py
@@ -1,6 +1,6 @@
 import subprocess
 import tarfile
-from datetime import datetime
+from datetime import datetime, timedelta
 from os import environ
 
 from minio import Minio
@@ -10,7 +10,8 @@ from prefect import flow, task
 @task
 def dumpall(PG_PASSWORD, PG_URI, name="pg-export"):
     cmd = f"PGPASSWORD={PG_PASSWORD} pg_dumpall -d postgres://{PG_URI} > {name}.sql"
-    subprocess.run(cmd, shell=True)
+    result = subprocess.run(cmd, shell=True, check=True)
+    result.check_returncode()
 
 
 @task
@@ -28,6 +29,25 @@ def uploadToS3(minioclient, name="pg-export"):
         f"{name}-{datetime.utcnow().isoformat()}.sql.tar.gz",
         f"{name}.sql.tar.gz",
     )
+    # delete local file
+    subprocess.run(f"rm {name}.sql.tar.gz", shell=True)
+
+
+@task
+def deleteOldFiles(miniclient, keep_for_days: int = 30):
+    fileByAge = dict()
+    allFiles = miniclient.list_objects(environ["MINIO_BUCKET"])
+    for file in allFiles:
+        filename = file.object_name
+        # split filename only at the first -
+        _, date = filename.split("-", 1)
+        # parse from isoformat
+        date = datetime.fromisoformat(date)
+        fileByAge[date] = filename
+    for createdDate, filename in fileByAge.items():
+        if createdDate < datetime.utcnow() - timedelta(days=keep_for_days):
+            print(f"deleting {filename} bc older than {keep_for_days} days")
+            miniclient.remove_object(environ["MINIO_BUCKET"], filename)
 
 
 @flow(log_prints=True)
@@ -47,6 +67,8 @@ def backupAllDatabases():
         URIS.append(database.split(","))
     for uri, name in URIS:
         try:
+            if "-" in name:
+                raise ValueError("Name must not contain -")
             password = uri.split(":")[1].split("@")[0]
             print(f"Backing up {name} with uri {uri}")
             dumpall(password, uri, name)
@@ -55,3 +77,9 @@ def backupAllDatabases():
         except Exception as e:
             print(f"Error while backing up {name}: {e}")
             continue
+    print("backup finished, deleting old files")
+    deleteOldFiles(client, int(environ.get("KEEP_FOR_DAYS", 30)))
+
+
+if __name__ == "__main__":
+    backupAllDatabases()

4e0fd626de695daf151143e339f2d1a26ab2d284
Justin Guese <guese.justin@gmail.com>
2023-12-28 15:55:03 +0100

    randombot

--- flows/addAllFlows.py
+++ flows/addAllFlows.py
@@ -10,9 +10,11 @@ import databaseBackup
 import finnhub_recommendations
 import headShoulderPrefect
 import msftDecisionTree
-import pairtrade_glri_vstm
-import pairtrade_uboh_glri
+
+# import pairtrade_glri_vstm
+# import pairtrade_uboh_glri
 import portoflioworthUpdate
+import randombot
 from prefect.client.schemas.schedules import CronSchedule
 from prefect.deployments import Deployment
 from tqdm import tqdm
@@ -22,6 +24,8 @@ FLOWS = [
     # main update functions
     (portoflioworthUpdate.portfolioWorthFlow, "0_portfolioworthupdate", "10 22 * * *"),
     (alphavantageUpdate.alphavantageUpdateFlow, "0_alphavantageupdate", "0 18 * * *"),
+    # database backup
+    (databaseBackup.backupAllDatabases, "0_database-backup", "0 3 * * *"),
     # composer
     (comp_bigtechmom.bigTechMomFlow, "composer-bigtechmomentum-v1", "42 3 * * *"),
     (comp_bondtqqq.compBondTQQQ, "composer-bondtqqq-v1", "15 22 * * *"),
@@ -48,8 +52,7 @@ FLOWS = [
     (msftDecisionTree.msftDecisionTree, "msft-decisiontree", "5 4 * * 3"),
     # (pairtrade_glri_vstm.pairTradeGLRIVSTM, "pairtrade-glri-vstm", "0 17 * * *"), # deactivated, shorting not implemented
     # (pairtrade_uboh_glri.pairTradeUBOHGLRI, "pairtrade-uboh-glri", "5 17 * * *"), # deactivated, shorting not implemented
-    # database backup
-    (databaseBackup.backupAllDatabases, "0_database-backup", "0 3 * * *"),
+    (randombot.randomBot, "randombot", "0 20 * * *"),
 ]
 
 for flow, name, cron in tqdm(FLOWS):
---
+++ flows/randombot.py
@@ -0,0 +1,67 @@
+from os import environ
+from random import randint, random
+
+import numpy as np
+import yfinance as yf
+from basebot22.basebot import BaseBot
+from prefect import flow, task, variables
+
+
+@flow(log_prints=True)
+def randomBot():
+    TRADEABLE = [
+        "AAPL",
+        "MSFT",
+        "GOOG",
+        "TSLA",
+        "AMD",
+        "AMZN",
+        "DG",
+        "KDP",
+        "LLY",
+        "NOC",
+        "NVDA",
+        "PGR",
+        "UNH",
+        "WM",
+        "CWEG.L",
+        "IWDA.AS",
+        "EEM",
+        "BTC-USD",
+        "ETH-USD",
+        "AVAX-USD",
+    ]
+
+    bot = BaseBot(
+        "randombot",
+        backendurl=environ.get("BACKEND_URL", variables.get("backend_url")),
+    )
+    portfolio = bot.getPortfolio()
+    oneSold = False
+    for ticker, amount in portfolio.items():
+        if ticker == "USD":
+            continue
+        if random() > 0.9:
+            print("selling all " + ticker)
+            bot.sell(ticker, -1)
+            oneSold = True
+    if oneSold:
+        portfolio = bot.getPortfolio()
+    usd = portfolio["USD"]
+
+    for ticker in TRADEABLE:
+        if usd < 10:
+            break
+        if random() > 0.9:
+            print("buying " + ticker)
+            if usd < 50:
+                amount = usd
+            else:
+                amount = random() * usd
+            print(f"buying {amount}$ of {ticker}")
+            bot.buy(ticker, amount)
+            usd -= amount
+
+
+if __name__ == "__main__":
+    randomBot()

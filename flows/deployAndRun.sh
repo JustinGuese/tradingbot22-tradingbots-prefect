bd274cc0a5cf6757b51685097dee29ab8afce25a
Justin Guese <guese.justin@gmail.com>
2023-12-29 20:14:04 +0100

    folder fix

--- flows/deployAndRun.sh
+++ flows/deployAndRun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python addAllFlows.py
 # little detour to add ai investing bots task as well
-cd ../tradingbot-website-generator/src/
+cd tradingbot-website-generator/src/
 python addFlowToPrefect.py
-cd ../../flows
+cd ../../
 prefect worker start --pool caprover-docker-container

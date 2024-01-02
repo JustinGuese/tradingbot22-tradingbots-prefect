b4e1796ed53fd0701f3d3d0b55ddd3d3bd12a6f5
Justin Guese <guese.justin@gmail.com>
2024-01-02 21:04:08 +0100

    adding hugo absolute path

--- flows/ai_investing_website.py
+++ flows/ai_investing_website.py
@@ -4,25 +4,28 @@ from datetime import datetime
 
 from prefect import flow, task, variables
 
+# must be absolute, because prefect uses tmpdirs
+HUGO_WORKDIR = "/app/flows/tradingbot-website-generator"
+
 
 @task
 def runPythonScripts():
+    global HUGO_WORKDIR
     # print current working directory
     print("current working directory: ", os.getcwd())
     subprocess.run(
         ["python", "getBotData.py"],
         capture_output=True,
-        cwd="tradingbot-website-generator/src/",
+        cwd=HUGO_WORKDIR + "/src/",
     )
 
 
 @task
 def hugoBuild():
+    global HUGO_WORKDIR
     # delete everything in "public" folder recursively
     try:
-        subprocess.run(
-            ["rm", "-rf", "public/*"], cwd="tradingbot-website-generator/hugo/"
-        )
+        subprocess.run(["rm", "-rf", "public/*"], cwd=HUGO_WORKDIR)
     except Exception as e:
         print(
             "could not delete contents of public folder, should be alright... " + str(e)
@@ -30,10 +33,10 @@ def hugoBuild():
 
     subprocess.run(
         ["hugo", "--minify", "-d", "public"],
-        cwd="tradingbot-website-generator/hugo/",
+        cwd=HUGO_WORKDIR,
     )
 
-    with open("tradingbot-website-generator/hugo/public/lastUpdate.txt", "w") as file:
+    with open(HUGO_WORKDIR + "/public/lastUpdate.txt", "w") as file:
         file.write(str(datetime.utcnow()))
 
 

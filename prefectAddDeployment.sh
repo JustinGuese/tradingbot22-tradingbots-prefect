2782bb69479c72ef4dc759bdb84e4256a800e096
Justin Guese <guese.justin@gmail.com>
2024-02-06 13:37:33 +0100

    gitlab repo

--- prefectAddDeployment.sh
+++ prefectAddDeployment.sh
@@ -6,7 +6,7 @@ for file in $ALLFILES; do
     filename="${filename%.*}" 
     # the file has CRON = "0 18 * * *" in the first line, extract the cron expression from it
     cron=$(head -n 1 "$file" | grep -o '".*"' | sed 's/"//g')
-    prefect deployment build "$file:mainFlow" -n $filename -q docker-caprover-container-sb -sb github/tradingbot-repo -p docker-caprover-container-sb --cron "$cron"
+    prefect deployment build "$file:mainFlow" -n $filename -q docker-caprover-container-sb -sb  gitlab-repository/tradingbot22-tradingbots-prefect -p docker-caprover-container-sb --cron "$cron"
     # removed docker infra as we are already running in a container -ib docker-container/docker-container-execution
     prefect deployment apply "mainFlow-deployment.yaml"
     # remove the deployment file
